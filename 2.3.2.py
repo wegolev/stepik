8a77f8939c3b283be73b90d357ac7d2a5738ed02
wegolev_p500ca <a.wegolev@gmail.com>
2023-06-16 04:30:42 +0300

    Before push

--- 2.3.2.py
+++ 2.3.2.py
@@ -1,12 +1,13 @@
 import sympy
 
 x, y, w, b = sympy.symbols('x y w b')
-res = sympy.diff(-y*sympy.log(1/(1+sympy.exp(-w*x-b)))-(1-y)*sympy.log(1-1/(1+sympy.exp(-w*x-b))), w)
+res = sympy.diff(-y*sympy.log(1/(1+sympy.exp(-w*x-b)))-(1-y)*sympy.log(1-1/(1+sympy.exp(-w*x-b))), b)
 print(str(sympy.simplify(res)))
 
 #import sympy.parsing.sympy_parser
 
-sample_expr_str = 'x*(-y*exp(b + w*x) - y + exp(b + w*x))/(exp(b + w*x) + 1)'
+#sample_expr_str = '(-y*exp(b + w*x) - y + exp(b + w*x))/(exp(b + w*x) + 1)'
+sample_expr_str = str(sympy.simplify(res))
 sample_expr = sympy.parsing.sympy_parser.parse_expr(sample_expr_str)
 sample_value = sample_expr.evalf(subs=dict(x=0.5, y=1, w=4, b=1))
 print(sample_value)
