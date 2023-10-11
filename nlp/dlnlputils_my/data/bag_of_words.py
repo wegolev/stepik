39f320ee5c234db847b3ba672d0563b08a5b2140
wegolev <a.wegolev@gmail.com>
2023-10-11 08:58:16 +0300

    change the way features are weighted: log(tf+1)idf

--- nlp/dlnlputils_my/data/bag_of_words.py
+++ nlp/dlnlputils_my/data/bag_of_words.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset
 
 
 def vectorize_texts(tokenized_texts, word2id, word2freq, mode='tfidf', scale=True):
-    assert mode in {'tfidf', 'idf', 'tf', 'bin'}
+    assert mode in {'log(tf)idf', 'tfidf', 'idf', 'tf', 'bin'}
 
     # считаем количество употреблений каждого слова в каждом документе
     result = scipy.sparse.dok_matrix((len(tokenized_texts), len(word2id)), dtype='float32')
@@ -13,6 +13,9 @@ def vectorize_texts(tokenized_texts, word2id, word2freq, mode='tfidf', scale=Tru
         for token in text:
             if token in word2id:
                 result[text_i, word2id[token]] += 1
+    # print(type(result))
+    # print(result.shape)
+    # print(result[0])
 
     # получаем бинарные вектора "встречается или нет"
     if mode == 'bin':
@@ -32,14 +35,20 @@ def vectorize_texts(tokenized_texts, word2id, word2freq, mode='tfidf', scale=Tru
     # частоту слова в документе и частоту слова в корпусе
     elif mode == 'tfidf':
         result = result.tocsr()
-        result = result.multiply(1 / result.sum(1))  # разделить каждую строку на её длину
+        result = result.multiply(1 / result.sum(1) + 1)  # разделить каждую строку на её длину
+        result = result.multiply(1 / word2freq)  # разделить каждый столбец на вес слова
+    
+    elif mode == 'log(tf)idf':
+        result = result.tocsr()
+        result = result.multiply(np.log1p(1 / result.sum(1) + 1))  # разделить каждую строку на её длину 
+        # result = np.log1p(result) # разделить каждую строку на её длину 
         result = result.multiply(1 / word2freq)  # разделить каждый столбец на вес слова
 
     if scale:
         result = result.tocsc()
         result -= result.min()
         result /= (result.max() + 1e-6)
-
+    
     return result.tocsr()
 
 
